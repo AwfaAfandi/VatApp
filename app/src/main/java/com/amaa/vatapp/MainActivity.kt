ced84909bf2e05ff19290d351331101d80ea4b73
Awfa Afandi <amaa.vip@gmail.com>
2022-12-04 13:23:27 +0300

    updated

--- app/src/main/java/com/amaa/vatapp/MainActivity.kt
+++ app/src/main/java/com/amaa/vatapp/MainActivity.kt
@@ -7,59 +7,43 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-
-    // TODO [1] Create binding object
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // TODO [2] Layout View Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root // reference layout file
-        setContentView(view) // link layout
+        val view = binding.root
+        setContentView(view)
 
-        // TODO [3] Button binding and Calculate VAT
         binding.calculateButton.setOnClickListener {
             calculateVat()
-        }// End setOnClickListener
+        }
+    }
+private fun calculateVat() {
 
-
-    }// End onCreate
-
-    private fun calculateVat() {
-
-        // TODO [4] receive and save the inputs
-
-        // EditText value
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
-
-        // Radio Group
         val selectedId = binding.tipOptions.checkedRadioButtonId
-
-        // Radio Button Selected
         val vatPercentage = when(selectedId){
             R.id.option_ten -> 0.10
             R.id.option_fifteen -> 0.15
             else -> 0.20
         }
-
         var vat = vatPercentage * cost
 
         var total = cost + vat
 
-        // Switch Value - True or False
+
         val roundVat = binding.roundUpSwitch.isChecked
 
         if (roundVat){
             total = kotlin.math.ceil(total)
         }
 
-        // Total Formatting
-        val formattedTotal = NumberFormat.getCurrencyInstance().format(total) // ربطت المبلغ بالعملة
+        val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
         binding.totaltext.text = getString(R.string.total_amount, formattedTotal)
 
     }
 
-}// End MainActivity
+}
